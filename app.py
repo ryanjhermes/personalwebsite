29c62b050f9e92c0aff4780e28c7719450db3d3c
Ryan Hermes <rryanhermes@gmail.com>
2024-09-27 15:06:30 -0500

    Is this the dagger?

--- app.py
+++ app.py
@@ -1,35 +1,16 @@
 from flask import Flask, render_template, send_from_directory
-import csv
+import functions
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
-
-# Function to read URLs from the CSV file
-def read_urls_from_csv(file_path):
-    data = []
-    with open(file_path, mode='r') as file:
-        csv_reader = csv.DictReader(file)
-        for row in csv_reader:
-            data.append({
-                'Website': row['Website'].strip(),
-                # 'Description': row['Description'].strip()  # Uncomment if you want to include the description
-            })
-    return data
-
-# Load data from CSV
-def load_data():
-    return read_urls_from_csv('companies2.csv')  # Ensure the path is correct
 
 @app.route("/")
 def home():
     return render_template('index.html')
 
 @app.route("/sourcing-tool")
-@app.route("/sourcing-tool")
 def sourcing_tool():
-    data = load_data()  # Load the data from the CSV file
+    data = functions.load_data()  # Load the data from the CSV file
     return render_template('sourcing_tool.html', data=data)  # Pass the data to the template
-
-
 
 @app.route("/resume")
 def resume():
